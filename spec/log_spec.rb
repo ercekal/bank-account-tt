d3fe67b303335290146265e58631c26b7f4fb4df
ercekal <ercekalabalikoglu@gmail.com>
2016-10-18 15:05:49 +0100

    adds test to log

--- spec/log_spec.rb
+++ spec/log_spec.rb
@@ -7,8 +7,8 @@ describe Log do
 subject(:log) { described_class.new}
 let (:account) { double :account}
 
-  # it 'should store the transactions' do
-  #   account.deposit(50)
-  #   expect(account.log.log).to include("18/10/2016 || 50 || || 50")
-  # end
+  it 'should store the transactions' do
+    log.add_to_log(50, 60)
+    expect(log.log).to include("18/10/2016 || 50 || || 60")
+  end
 end
